83369f16a8558d784cef89aafca06edc69bd8f83
10sandeep <sandeepnayak1424@gmail.com>
2025-01-03 10:03:59 +0530

    how to write nested code in react  using javascript

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
       // it is just creating h1 element inside react,its a core thing of react
 
     //   {} is an object used for attribute
-      const heading = React.createElement("h1", {id:"heading"} ,"Namaste React! ");
+      // const heading = React.createElement("h1", {id:"heading"} ,"Namaste React! ");
       //  when we are creating a root to rendering something inside it is a job of react dom
       //   when we wants to put our h1 inside the dom or browser we need react dom
-         const root = ReactDOM.createRoot(document.getElementById("root"));
-         root.render( heading);
+        //  const root = ReactDOM.createRoot(document.getElementById("root"));
+        //  root.render( heading);
 
         //   
+
+       
+        const parent=React.createElement("div", {id:"parent"},
+          React.createElement("div" ,{id:"child"},
+          React.createElement("h1" ,{},"i am the heading") ) );
+          console.log(parent)
+
+        const root = ReactDOM.createRoot(document.getElementById("root"));
+        root.render(parent);
