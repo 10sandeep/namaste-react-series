73bc7d467d5bad7e7da41f6b1c0102f4c4ec9b6a
10sandeep <sandeepnayak1424@gmail.com>
2025-01-04 23:27:14 +0530

    end of episode 1

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-      // it is just creating h1 element inside react,its a core thing of react
+    
 
     //   {} is an object used for attribute
       // const heading = React.createElement("h1", {id:"heading"} ,"Namaste React! ");
-      //  when we are creating a root to rendering something inside it is a job of react dom
+      //  when we are creating a root to rendering something inside it, it is a job of react dom
       //   when we wants to put our h1 inside the dom or browser we need react dom
         //  const root = ReactDOM.createRoot(document.getElementById("root"));
+        
+       // when we are write root.render(parent) it just take the parent as object and create a dom element inside the root element which the browser can understand.
+       //here heading is known as react element or javascript object
         //  root.render( heading);
 
         //   
@@ -16,6 +19,7 @@
         //   console.log(parent)
 
         // const root = ReactDOM.createRoot(document.getElementById("root"));
+
         // root.render(parent);
 
         //creating a siblings you can convert it into an array of children
@@ -39,5 +43,5 @@
           console.log(parent)
 
         const root = ReactDOM.createRoot(document.getElementById("root"));
-        root.render(parent);
+        root.render(parent); //it is just put the parent inside the root,and if something already there inside the root tag then it will replace it with the parent
 
